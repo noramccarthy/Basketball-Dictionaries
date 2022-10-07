2d3e1a0133ad64ea7931dc6d5fbf4ed218b42b49
Nora McCarthy <nzmccarthy16@gmail.com>
2022-10-06 21:37:41 -0400

    first commit

---
+++ Basketball_Dictionaries.py
@@ -0,0 +1,32 @@
+class Player:
+    def __init__(self, data):
+        self.name = data['name']
+        self.age = data['age']
+        self.position = data['position']
+        self.team = data['team']
+
+kevin = {
+        "name": "Kevin Durant", 
+        "age":34, 
+        "position": "small forward", 
+        "team": "Brooklyn Nets"
+}
+jason = {
+        "name": "Jason Tatum", 
+        "age":24, 
+        "position": "small forward", 
+        "team": "Boston Celtics"
+}
+kyrie = {
+        "name": "Kyrie Irving", 
+        "age":32, "position": "Point Guard", 
+        "team": "Brooklyn Nets"
+}
+    
+player_kevin=Player(kevin)
+print(player_kevin)
+player_jason=Player(jason)
+print(player_jason)
+player_kyrie=Player(kyrie)
+print(player_kyrie)
+
